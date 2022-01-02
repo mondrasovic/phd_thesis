2526b9dc8c97b22cb73ad13dc94b5e604033a393
Milan Ondrašovič <milan.ondrasovic@gmail.com>
2022-01-02 07:52:03 +0100

    Add script for listing unused labels.

---
+++ tools/unused_labels_founder.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# A script that finds unused citations within multipe "*.tex" files according to
+# some "*.bib" files.
+
+# Author: Milan Ondrasovic, <milan.ondrasovic@gmail.com>
+
+cd ..
+
+get_declared_labels() {
+    grep --include="*.tex" -Rho -P "label{[a-zA-Z0-9_:]*}" |\
+        cut -c 7- |\
+        rev | cut -c 2- | rev |\
+        sort 
+}
+
+get_used_labels() {
+    grep --include="*.tex" -Rho -P "ref{[a-zA-Z0-9_:]*}" |\
+        cut -c 5- |\
+        rev | cut -c 2- | rev |\
+        sort | uniq
+}
+
+# Report different lines only from the left source.
+comm -23 <(get_declared_labels) <(get_used_labels)
